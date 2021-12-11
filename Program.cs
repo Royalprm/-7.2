9a180478669f0c2adff298074d426becb4d274dd
Royalprm <95142405+Royalprm@users.noreply.github.com>
2021-12-11 18:18:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Упражнение_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube MS;
+            Console.WriteLine("Введите длину ребра куба:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            MS = new Cube(a);
+            Console.WriteLine("Объем Куба = " + MS.V(a) + " Площадь поверхности = " + MS.S(a));
+
+            Console.ReadKey();
+        }
+    }
+    class Cube
+    {
+        public int A { get; set; }
+
+        public Cube(int a)
+        {
+            A = a;
+        }
+        public int S(int g)
+        {
+            return 6 * g * g;
+        }
+        public int V(int g)
+        {
+            return g * g * g;
+        }
+
+    }
+}
